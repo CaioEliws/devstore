a4d78b9e51e02d0d55dd1e0b6e200f836dceaa23
caio <caioeliasvieira2@gmail.com>
2024-03-29 12:36:39 -0300

    data search created in details

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -63,7 +63,9 @@ export default async function Home() {
 
               <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
                 <span className="text-sm truncate">{product.title}</span>
-                <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">{product.price.toLocaleString("pt-br",{
+                <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold"
+                >
+                  {product.price.toLocaleString("pt-br",{
                   style: "currency",
                   currency: 'brl',
                 })}
--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,11 +1,33 @@
+import { Product } from "@/data/types/product";
+import { api } from "@/data/api";
 import Image from "next/image";
 
-export default function ProductPage() {
+interface ProductProps {
+    params: {
+        slug: string
+    }
+}
+
+async function getProduct(slug: string): Promise<Product> {
+    const response = await api(`/products/${slug}`, {
+      next: {
+        revalidate: 60 * 60, // atualizado a cada 1 hora o cache
+      },
+    })
+  
+    const product = await response.json()
+  
+    return product
+}
+
+export default async function ProductPage({ params }: ProductProps) {
+    const product = await getProduct(params.slug)
+
     return (
         <div className="relative grid max-h-[860px] grid-cols-3">
             <div className="col-span-2 overflow-hidden">
                 <Image
-                    src="/moletom-never-stop-learning.png"
+                    src={product.image}
                     alt=""
                     width={1000}
                     height={1000}
@@ -15,22 +37,28 @@ export default function ProductPage() {
 
             <div className="flex flex-col justify-center px-12">
                 <h1 className="text-3xl font-bold leading-tight ">
-                    Moletom Never Stop Learning
+                    {product.title}
                 </h1>
 
 
                 <p className="pt-2 leading-relaxed text-zinc-400">
-                    Moletom fabricado com 88% de algodão e 12% de poliéster.
+                    {product.description}
                 </p>
 
 
                 <div className="mt-8 flex items-center gap-3">
                     <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-                        R$129
+                        {product.price.toLocaleString("pt-br",{
+                            style: "currency",
+                            currency: 'brl',
+                        })}
                     </span>
 
                     <span className="text-sm text-zinc-400">
-                        Em 12x s/ juros de R$10,75
+                        {(product.price / 12).toLocaleString("pt-br",{
+                            style: "currency",
+                            currency: 'brl',
+                        })}
                     </span>
                 </div>
 
