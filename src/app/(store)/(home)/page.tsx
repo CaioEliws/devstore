287645071f06a3ccc0d36b6f5e6deca8e733f52a
caio <caioeliasvieira2@gmail.com>
2024-03-29 09:53:04 -0300

    Create my api

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,7 +1,21 @@
+import { api } from "@/data/api";
+
 import Image from "next/image";
 import Link from "next/link";
 
+async function getFeaturedProducts() {
+  const response = await api("/products/featured")
+
+  const products = await response.json()
+
+  return products
+}
+
 export default async function Home() {
+  const products = await getFeaturedProducts()
+
+  console.log(products)
+
   return (
       <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
         <Link 
