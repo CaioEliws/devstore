f8905e94f6f79e60ec4b2d923d58a76c23a3ff67
caio <caioeliasvieira2@gmail.com>
2024-03-29 10:41:02 -0300

    put Cache & Memorization update every 1 hour

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -4,8 +4,14 @@ import { Product } from "@/data/types/product";
 import Image from "next/image";
 import Link from "next/link";
 
+
+
 async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api("/products/featured")
+  const response = await api("/products/featured", {
+    next: {
+      revalidate: 60 * 60, // atualizado a cada 1 hora o cache
+    },
+  })
 
   const products = await response.json()
 
